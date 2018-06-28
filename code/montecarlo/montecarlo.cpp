2a6e34acb2b7aaa77c701fe9290499f52b8c7590
Nils <33125441+nilsspeetzen@users.noreply.github.com>
2018-06-28 17:33:22 +0200

    asd

--- code/montecarlo/montecarlo.cpp
+++ code/montecarlo/montecarlo.cpp
@@ -10,32 +10,31 @@ int main(int c, char* v[]) {
     cout << endl << "-----Menue-----" << endl
                  << "1: System loesen" << endl
                  << "2: Ergebnisse anzeigen" << endl;
-    int n=0;
-    cin >> n;
+    int n=1;
+    //cin >> n;
     switch (n) {
     case 1:
     {
-        string input = "";
-        cout << "Inputdatei: ";
-        cin >> input;
-        string test = "";
-        ifstream inputstream;
-        inputstream.open (input);
-        inputstream >> test;
-        inputstream.close();
+        string input = "m.txt";
+        //cout << "Inputdatei: ";
+        //cin >> input;
+        MT firstline;
+        ifstream infile(input);
+        int n, m;
+        infile >> n >> m;
+        LINEAR_SYSTEM lsys(n);
+        lsys.A() = MT::Constant(n,m,0);
 
-        cout << test << endl;
-        /*LINEAR_SYSTEM lsys(n);
-        lsys.A()=MT::Random(n,n);
-        lsys.b()=VT::Random(n);
-        cout << "LU" << endl;
-        LU lsol;
-        lsol.solve(lsys);
-        cout << "x=" << endl << lsys.x() << endl;*/
+        for(int i=0; i<n;i++)
+            for(int j=0; j<m;j++) {
+                infile >> lsys.A(i,j);
+            }
+
+        cout << lsys.A() << endl;
+
         break;
     }
     default:
-        cout << endl << "-----Fehler-----" << endl;
         break;
     }
 
